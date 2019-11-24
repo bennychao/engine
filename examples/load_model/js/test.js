877650c0fd7b4f87f9ee4b2188203e0732dbdb50
bennychao <83353654@qq.com>
2019-11-24 23:09:43 +0800

    change the UI

--- examples/load_model/js/test.js
+++ examples/load_model/js/test.js
@@ -1,12 +1,27 @@
-var select = 1;
+var selectCard = 1;
 
 var app1 = new Vue({
     el: '#app',
     data: {
       sites: [
-        { name: 'Runoob', id :'1', selected: {'btn-my-focus': false}},
-        { name: 'Google', id : '2', selected: {'btn-my-focus': false}},
-        { name: 'Taobao', id : '3', selected: {'btn-my-focus': false}}
+        { name: 'Runoob', id :'1', isRecommend: {'is-recommend': true}},
+        { name: 'Google', id : '2'},
+        { name: 'Taobao', id : '3', isFocused: {'is-focused': false}},
+        { name: 'Google', id : '4'},
+        { name: 'Google', id : '5'},
+        { name: 'Google', id : '6'},
+        { name: 'Google', id : '7'},
+        { name: 'Google', id : '8'},
+        { name: 'Google', id : '9'},
+        { name: 'Google', id : '10'},
+        { name: 'Google', id : '11'},
+        { name: 'Google', id : '12'},
+        { name: 'Google', id : '13'},
+        { name: 'Google', id : '14'},
+        { name: 'Google', id : '15'},
+        { name: 'Google', id : '16'},
+        { name: 'Google', id : '17'},
+        { name: 'Google', id : '18'}
       ]
     },
     methods: {
@@ -17,12 +32,44 @@ var app1 = new Vue({
         // if (event) {
         //     alert(event.target.tagName)
         // }
-        select =new Number(event.target.id);
+        selectCard =new Number(event.target.id);
 
         app1.sites.forEach(element => {
-          element.selected = {'btn-my-focus': false};
+          element.isFocused = {'is-focused': false};
         });
-        app1.sites[select - 1].selected = {'btn-my-focus': true};
+        app1.sites[selectCard - 1].isFocused = {'is-focused': true};
+      }
+    }
+  })
+
+
+  var selectCardShow = 1;
+
+var app1 = new Vue({
+    el: '#app2',
+    data: {
+      sites: [
+        { name: 'Runoob', id :'1', isRecommend: {'is-recommend': true}},
+        { name: 'Google', id : '2'},
+        { name: 'Taobao', id : '3', isFocused: {'is-focused': false}},
+        { name: 'Google', id : '4'},
+        { name: 'Google', id : '5'}
+      ]
+    },
+    methods: {
+      click: function (event) {
+        // `this` 在方法里指当前 Vue 实例
+         //alert('Hello ' + this.name + '!')
+        // // `event` 是原生 DOM 事件
+        // if (event) {
+        //     alert(event.target.tagName)
+        // }
+        selectCardShow =new Number(event.target.id);
+
+        app1.sites.forEach(element => {
+          element.isFocused = {'is-focused': false};
+        });
+        app1.sites[selectCardShow - 1].isFocused = {'is-focused': true};
       }
     }
   })
