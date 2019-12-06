6fcf54c96af9e81d6b54a965c81749c5523fdf36
zhao bin <83353654@qq.com>
2019-12-06 17:17:27 +0800

    change feature UI

--- examples/Test/testFeature.js
+++ examples/Test/testFeature.js
@@ -15,15 +15,15 @@ var vm = new Vue({
                 name: '优惠券3'
             }
         ],
-        couponSelected: '',
+        couponSelected: '1',
     },
     created(){
 　　　　　　　　　　　　//如果没有这句代码，select中初始化会是空白的，默认选中就无法实现
-        this.couponSelected = this.couponList[0].id;
+        //this.couponSelected = this.couponList[0].id;
     },
     methods:{
 
-　　　　　　　　　　　　getCouponSelected(){
+　　　　　　　getCouponSelected(){
             //获取选中的优惠券
             console.log(this.couponSelected)
         }
